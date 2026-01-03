1149350ff30d0f4e531560d8da71d8d14c3cf741
Anandu Udayan <anandhuudayan180@gamil.com>
2026-01-03 15:55:07 +0530

    feat: implement UI for bloc demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
+import 'package:bloc_counter_app_demo/home/counter_page/counter_page.dart';
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: CounterPage());
+  }
+}
+
